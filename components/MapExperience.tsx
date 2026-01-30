d63cd59dbd4be3d66e40fbaddae46b3fc26dca9d
nynysaf <naryan@gmail.com>
2026-01-30 16:20:55 -0500

    Fix jsPDF: use positional constructor (p, px, [w,h]) for custom page size

--- components/MapExperience.tsx
+++ components/MapExperience.tsx
@@ -479,11 +479,7 @@ const MapExperience: React.FC<MapExperienceProps> = ({
 
           if (exportFormat === 'pdf') {
             const imgData = canvas.toDataURL('image/png');
-            const pdf = new jsPDF({
-              orientation: 'square',
-              unit: 'px',
-              format: [canvas.width, canvas.height],
-            });
+            const pdf = new jsPDF('p', 'px', [canvas.width, canvas.height]);
             pdf.addImage(imgData, 'PNG', 0, 0, canvas.width, canvas.height);
             pdf.save(filename);
           } else {
